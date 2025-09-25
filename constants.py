b8cc94012304e1b15edf13a9eacf007a1b9dafef
emmtt14 <22488@mycva.org>
2025-09-25 08:24:19 -0700

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,26 @@
+# constants.py
+
+# Game State Constants
+STATE_MAIN_MENU = "MAIN_MENU"
+STATE_NEW_GAME = "NEW_GAME"
+STATE_LOAD_GAME = "LOAD_GAME"
+STATE_DRAFT = "DRAFT"
+STATE_SEASON = "SEASON"
+STATE_GAMEPLAY = "GAMEPLAY"
+STATE_END_GAME = "END_GAME"
+STATE_EXITING = "EXITING"
+
+# Draft Constants
+DEFAULT_DRAFT_PLAYERS = 20
+DEFAULT_NUMBER_OF_TEAMS = 2
+
+# Player Attribute Ranges
+ATTRIBUTE_MIN = 50
+ATTRIBUTE_MAX = 99
+
+# Position Definitions
+OFFENSIVE_POSITIONS = ["QB", "RB", "WR", "WR", "TE", "C", "G", "G", "T", "T"]
+DEFENSIVE_POSITIONS = ["DE", "DE", "DT", "DT", "LB", "LB", "CB", "CB", "S", "S"]
+
+# Season Constants
+DEFAULT_SEASON_WEEKS = 3 # For demonstration, a short season
